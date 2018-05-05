11191ca4ebc152aa76079be71973224cc9a8e972
Bang Dao <bang@inspectorio.com>
2018-05-05 22:37:23 +0700

    display version by user

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils import timezone
 from django.views.generic import TemplateView
 from rest_framework import viewsets, permissions
@@ -11,6 +12,11 @@ class ServiceDeployViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceDeploySerializer
     permission_classes = (permissions.IsAuthenticated,)
 
+    def get_queryset(self):
+        queryset = super(ServiceDeployViewSet, self).get_queryset()
+        queryset = queryset.filter(user=self.queryset.user)
+        return queryset
+
     def perform_create(self, serializer):
         data = serializer.validated_data
         try:
@@ -23,12 +29,17 @@ class ServiceDeployViewSet(viewsets.ModelViewSet):
             service_deploy.save()
 
 
-class IndexView(TemplateView):
+class IndexView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/index.html'
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+
+        service_deploys = ServiceDeploy.objects.filter(user=self.request.user).order_by('name')
+        environments = sorted(set(service_deploys.values_list('environment', flat=True)))
+
         context.update({
-            'service_deploys': ServiceDeploy.objects.all().order_by('name'),
+            'service_deploys': service_deploys,
+            'environments': environments,
         })
         return context
