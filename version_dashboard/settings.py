64530dbf3f38973800709e45d3680d6165b00452
Bang Dao <bang@inspectorio.com>
2018-05-05 10:11:13 +0700

    init accounts app for user

--- version_dashboard/settings.py
+++ version_dashboard/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# User model
+# https://docs.djangoproject.com/en/2.0/ref/settings/#std:setting-AUTH_USER_MODEL
+
+AUTH_USER_MODEL = 'accounts.User'
